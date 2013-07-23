281e5b80b99d6a4003e8405d95792974af3a5471
uglybugger <andrewh@uglybugger.org>
2013-07-23 20:36:08 +1000

    Added another test. Marked as inconclusive as it's our starting point for tomorrow :)

---
+++ Source/MLAA.UnitTests/WhenAStudentAttemptsToEnrolInASubjectThatIsFull.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MLAA.Data.Linq2Sql;
+using NUnit.Framework;
+
+namespace MLAA.UnitTests
+{
+    [TestFixture]
+    public class WhenAStudentAttemptsToEnrolInASubjectThatIsFull
+    {
+        [Test]
+        [ExpectedException(typeof (InvalidOperationException))]
+        public void TheyShouldNotBePermitted()
+        {
+            var fred = new Student();
+            var law = new Subject
+            {
+                MaxStudents = 0,
+            };
+
+            fred.EnrolIn(law);
+
+            Assert.Inconclusive("This is our mission for tomorrow morning. We need to add a non-default constructor to our entity so that we can reliably create instances in a valid state.");
+        }
+    }
+}
