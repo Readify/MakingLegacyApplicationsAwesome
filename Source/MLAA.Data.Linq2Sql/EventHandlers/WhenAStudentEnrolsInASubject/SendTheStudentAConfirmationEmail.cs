eea3ab3d2fe184d9b0645e493e7b02da138ce161
uglybugger <andrewh@uglybugger.org>
2013-07-24 10:35:53 +1000

    Added our first event handler but haven't wired it up yet.

---
+++ Source/MLAA.Data.Linq2Sql/EventHandlers/WhenAStudentEnrolsInASubject/SendTheStudentAConfirmationEmail.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+
+namespace MLAA.Data.Linq2Sql.EventHandlers.WhenAStudentEnrolsInASubject
+{
+    public class SendTheStudentAConfirmationEmail: IHandle<StudentEnrolledInSubjectEvent>
+    {
+        private readonly IEmailSender _emailSender;
+
+        public SendTheStudentAConfirmationEmail(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        public void Handle(StudentEnrolledInSubjectEvent domainEvent)
+        {
+            var message = new MailMessage("from@example.com",
+                "to@example.com",
+                "Subject",
+                "Congratulations");
+
+            _emailSender.Send(message);
+        }
+    }
+
+    public interface IEmailSender
+    {
+        void Send(MailMessage message);
+    }
+
+    public interface IHandle<TDomainEvent>
+    {
+        void Handle(TDomainEvent domainEvent);
+    }
+}
