5efff270be402cc275f4771a6318b88e35acecdc
uglybugger <andrewh@uglybugger.org>
2013-07-24 12:08:36 +1000

    As a subject coordinator I want to receive a confirmation email when a student enrols in one of my subjects.

---
+++ Source/MLAA.Data.Linq2Sql/EventHandlers/WhenAStudentEnrolsInASubject/SendTheSubjectCoordinatorANotificationEmail.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Mail;
+
+namespace MLAA.Data.Linq2Sql.EventHandlers.WhenAStudentEnrolsInASubject
+{
+    public class SendTheSubjectCoordinatorANotificationEmail : IHandle<StudentEnrolledInSubjectEvent>
+    {
+        private readonly IEmailSender _emailSender;
+
+        public SendTheSubjectCoordinatorANotificationEmail(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        public void Handle(StudentEnrolledInSubjectEvent domainEvent)
+        {
+            var message = new MailMessage(
+                "from@example.com",
+                "coordinator@example.com",
+                "Another unwashed student has enrolled in your subject",
+                string.Format("{0} {1} has enrolled. Hooray.",
+                              domainEvent.Student.FirstName,
+                              domainEvent.Student.LastName));
+
+            _emailSender.Send(message);
+        }
+    }
+}
